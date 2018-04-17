98dc79d78b2134ff9abee48ca76c3404638ca990
paradise999 <32838010+paradise999@users.noreply.github.com>
2018-04-18 02:40:01 +0300

    Initial commit

---
+++ Lab7/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.search = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.farther = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(40, 125);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(801, 322);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(355, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(474, 49);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(75, 23);
+            this.search.TabIndex = 2;
+            this.search.Text = "Найти";
+            this.search.UseVisualStyleBackColor = true;
+            this.search.Click += new System.EventHandler(this.search_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(766, 49);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 3;
+            this.Clear.Text = "Очистить";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите слово для поиска";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Вставте текст";
+            // 
+            // farther
+            // 
+            this.farther.Location = new System.Drawing.Point(611, 49);
+            this.farther.Name = "farther";
+            this.farther.Size = new System.Drawing.Size(75, 23);
+            this.farther.TabIndex = 6;
+            this.farther.Text = "Далее";
+            this.farther.UseVisualStyleBackColor = true;
+            this.farther.Click += new System.EventHandler(this.farther_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 520);
+            this.Controls.Add(this.farther);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button search;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button farther;
+    }
+}
+
---
+++ Lab7/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab7
+{
+    public partial class Form1 : Form
+    {
+        Color colSel;
+        Color colSeBack;
+        string str;
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            colSel = richTextBox1.SelectionColor;
+            colSeBack = richTextBox1.SelectionBackColor;
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void search_Click(object sender, EventArgs e)
+        {
+           str = textBox1.Text;
+           i = 0;
+            if (i <= richTextBox1.Text.Length - str.Length)
+            {
+                i = richTextBox1.Text.IndexOf(str, i);
+                if (i >= 0)
+                {
+                    richTextBox1.SelectionStart = i;
+                    richTextBox1.SelectionLength = str.Length;
+                    richTextBox1.SelectionBackColor = Color.Yellow;
+                    richTextBox1.SelectionColor = Color.Black;                    
+                }
+                if (i == -1)
+                {
+                    MessageBox.Show("Такого в тексте нет");
+                }
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            textBox1.Clear();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionColor = colSel;
+            richTextBox1.SelectionBackColor = colSeBack;
+
+        }
+
+        private void farther_Click(object sender, EventArgs e)
+        {
+            if (i <= richTextBox1.Text.Length - str.Length)
+            {
+                if (i >= 0)
+                {
+                    richTextBox1.SelectionStart = i;
+                    richTextBox1.SelectionLength = str.Length;
+                    richTextBox1.SelectionBackColor = Color.White;
+                    richTextBox1.SelectionColor = Color.Black;
+                    i += str.Length;
+                    i = richTextBox1.Text.IndexOf(str, i);
+                }
+                if (i >= 0)
+                {
+                    richTextBox1.SelectionStart = i;
+                    richTextBox1.SelectionLength = str.Length;
+                    richTextBox1.SelectionBackColor = Color.Yellow;
+                    richTextBox1.SelectionColor = Color.Black;                   
+                }
+                if (i == -1)
+                {
+                    MessageBox.Show("Далее такого в тексте нет");
+                }
+            }
+        }
+    }
+}
